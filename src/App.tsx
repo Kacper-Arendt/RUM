94213f4fdbd497c2d4207f17312b722006f3677c
kacper-arendt <kacykvaa@gmail.com>
2021-10-11 08:50:22 +0200

    style: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import {createGlobalStyle} from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+    width: 100%;
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    box-sizing: border-box;
+    line-height: 150%;
+  }
+`;
+
 
 function App() {
-  return (
-  <></>
-  );
+    return (
+        <>
+            <GlobalStyle/>
+        </>
+    );
 }
 
 export default App;
