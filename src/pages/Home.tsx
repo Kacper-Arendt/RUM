7dc89550419ee75f47fa6cffadbe56d4d3afaf18
kacper.arendt <kacper.arendt@inspirelabs.pl>
2021-10-30 11:10:23 +0200

    feat: add description

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,12 @@ const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 1rem;
-  margin-top: 5rem;
+  max-width: 80rem;
+  margin: 5rem 0;
   padding: 0 2.5rem;
+
   
-  h3{
+  h3 {
     font-size: 2rem;
     margin-bottom: 1.5rem;
   }
@@ -17,24 +19,31 @@ const P = styled.p`
   line-height: 170%;
   font-size: 1.5rem;
   letter-spacing: 1.7px;
-  
-  :last-of-type{
+
+  :nth-of-type(3) {
     align-self: end;
+  }
+  :nth-of-type(4){
+    margin-top: 10rem;
+    font-size: .6rem;
   }
 `
 
 export const Home = () => {
+
     return (
         <StyledWrapper>
             <h3>Cześć!</h3>
-            <P> RUM czyli Rowerowe Ultra Maratony. Od 3 lat jestem fanem ultra, postanowiłem stworzyć miejsce w
-                sieci
-                gdzie, każdy będzie miał możliwość sprawdzenia informacji na temat wszystkich ultra w Polsce.
+            <P>
+                RUM czyli Rowerowe Ultra Maratony. Od 3 lat jestem fanem ultra, postanowiłem stworzyć miejsce w sieci
+                gdzie, każdy będzie miał możliwość sprawdzenia informacji na temat każdego ultra w Polsce*.
+            </P>
+            <P>
                 Strona z każdym tygodniem będzie rozbudowana o kolejne funkcjonalności. Na początku chciałbym skupić
-                się
-                na zamieszczeniu informacji na temat rozpoczęcia zapisów i startów poszczególnych imprez.
+                się na zamieszczeniu informacji na temat rozpoczęcia zapisów i startów poszczególnych imprez.
             </P>
             <P>PozdRower, Kacper</P>
+            <P>*Przynajmniej tego gravelowego</P>
         </StyledWrapper>
     )
 }
